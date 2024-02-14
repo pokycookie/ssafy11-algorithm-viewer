0a378f4ae521665aff6eb0a6352818b459d4162f
pokycookie <0cookieboy0@gmail.com>
2024-02-14 20:19:29 +0900

    feat: github repo content clipboard

---
+++ app/api/[week]/route.ts
@@ -0,0 +1,19 @@
+import { Octokit } from 'octokit'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const octokit = new Octokit({
+  auth: process.env.OCTOKIT_AUTH,
+})
+
+export async function GET(req: Request) {
+  const week = req.url.split('api/')[1]
+  const res = await octokit.request('GET /repos/{owner}/{repo}/contents/{path}', {
+    owner: 'ssafy11-study',
+    repo: 'test',
+    path: `src/${week}`,
+  })
+  const data = res.data
+  return Response.json({ data })
+}
